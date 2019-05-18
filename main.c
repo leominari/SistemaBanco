8b02c777ef72407cdb46a24cf13f182ae38c6353
leominari <leo_minari@hotmail.com>
2019-05-18 13:29:26 -0300

    v0.4

--- main.c
+++ main.c
@@ -2,28 +2,11 @@
 #include <windows.h>
 
 int main(){
-    int op = 7;
-    do{
-        printf("1. OP\n");
-        printf("2. OP\n");
-        printf("3. OP\n");
-        printf("4. OP\n");
-        printf("5. OP\n");
-        printf("6. OP\n");
-        printf("0. OP\n");
-        scanf("%d", &op);
+    int ag = 0;
+    int conta = 0;
+        printf("Bem vindo ao Hulema\nRealize Login\n")
+        printf("Agencia: ");
+        printf("Conta: ");
 
-        switch (op)
-        {
-        case 1:
-            funcao();
-            break;
-        
-        default:
-            break;
-        }
-
-        
-    }while(op != 0);
     return 0;
 }
