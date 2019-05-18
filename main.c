6e9f9e213aae351c99da5c900328e9c61fa74827
leominari <leo_minari@hotmail.com>
2019-05-18 13:36:13 -0300

    v0.5 - Estrutura Projeto

--- main.c
+++ main.c
@@ -4,9 +4,14 @@
 int main(){
     int ag = 0;
     int conta = 0;
+    int senha = 0;
         printf("Bem vindo ao Hulema\nRealize Login\n")
         printf("Agencia: ");
+        scanf("%d",&ag);
         printf("Conta: ");
-
+        scanf("%d",&conta);
+        printf("Senha: ")
+        scanf("%d", &senha);
+        login(ag, conta, senha);
     return 0;
 }
